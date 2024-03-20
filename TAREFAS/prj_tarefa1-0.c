8266749d6f42394f78c23b3c0f26846bfd4ef5f6
Flavio Junior <161609104+flaviojuniordev@users.noreply.github.com>
2024-03-19 22:29:19 -0300

    Add files via upload

---
+++ TAREFAS/prj_tarefa1-0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int idade = 0;
+    printf("Valor inicial da sua idade: %d \n", idade);
+
+    printf("Digite a sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Sua idade é: %d \n", idade);
+}
