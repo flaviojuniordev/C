75dde11f1cf18dccfb0930b3819ea661ac072e82
Flavio Junior <161609104+flaviojuniordev@users.noreply.github.com>
2024-04-03 12:02:55 -0300

    Add files via upload

---
+++ prj_palindromo/src/prj_palindromo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    char palavra[100];
+    int i, j, tamanho;
+    int palindromo = 1;
+
+    printf("Digite uma palavra: ");
+    fflush(stdout);
+    scanf("%s", palavra);
+
+    tamanho = strlen(palavra);
+
+    // Verificar se é um palíndromo
+    for(i = 0, j = tamanho - 1; i < j; i++, j--) {
+
+    	printf("i: %c\n", palavra[i]);
+    	printf("j: %c\n", palavra[j]);
+
+        if(palavra[i] != palavra[j]) {
+            palindromo = 0;
+            break;
+        }
+    }
+
+    if(palindromo) {
+        printf("\nA palavra é um palíndromo.\n");
+    } else {
+        printf("\nA palavra não é um palíndromo.\n");
+    }
+
+    return 0;
+}
